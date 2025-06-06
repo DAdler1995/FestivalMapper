d983bb64fe7902199786ed68e96e2929b5d6a3ff
Dakota <Dakota@DESKTOP-CJS08FC>
2025-06-05 21:07:58 -0700

    Added basic festival image uploading, naming, and description. Began working on the edit stage logic.

---
+++ FestivalMapper.App/Models/DomRect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestivalMapper.App.Models
+{
+    public class DomRect
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}
---
+++ FestivalMapper.App/Services/ImagePickerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestivalMapper.App.Services
+{
+    public class ImagePickerService
+    {
+        public async Task<string?> PickImageAsBase64Async()
+        {
+			try
+			{
+				var file = await FilePicker.Default.PickAsync(new PickOptions
+				{
+					PickerTitle = "Select Festival Map Iamge",
+					FileTypes = FilePickerFileType.Images
+				});
+
+				if (file == null)
+				{
+					throw new Exception("Invalid Image Seclected");
+                }
+
+				using var Stream = await file.OpenReadAsync();
+				using var ms = new MemoryStream();
+
+				await Stream.CopyToAsync(ms);
+				var bytes = ms.ToArray();
+
+				return Convert.ToBase64String(bytes);
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error picking image: {ex.Message}");
+            }
+
+            return null;
+        }
+    }
+}
