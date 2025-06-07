cbf842632af1bc21a1ce3102d395d74eaf8eb5b9
Dakota <Dakota@DESKTOP-CJS08FC>
2025-06-06 23:59:37 -0700

    Potential fix for the keyboard overlap issue - TESTING NEEDED.

--- FestivalMapper.App/Platforms/Android/MainActivity.cs
+++ FestivalMapper.App/Platforms/Android/MainActivity.cs
@@ -1,11 +1,18 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using FestivalMapper.App.Platforms.Android;
 
 namespace FestivalMapper.App
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            GlobalLayoutUtil.Initialize();
+        }
+
     }
 }
