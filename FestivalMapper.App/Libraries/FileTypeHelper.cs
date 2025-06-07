fbca98fa203862dfce4c9b3175373d2be84a46d8
Dakota <Dakota@DESKTOP-CJS08FC>
2025-06-06 19:13:21 -0700

    Created the FestivalMap file type helper.

--- FestivalMapper.App/Libraries/FileTypeHelper.cs
+++ FestivalMapper.App/Libraries/FileTypeHelper.cs
@@ -4,10 +4,18 @@
     {
         public static readonly FilePickerFileType Json = new(new Dictionary<DevicePlatform, IEnumerable<string>>
         {
-            { DevicePlatform.WinUI, new[] { ".festivalmap" } },
+            { DevicePlatform.WinUI, new[] { ".json" } },
             { DevicePlatform.macOS, new[] { "public.json" } },
             { DevicePlatform.iOS, new[] { "public.json" } },
             { DevicePlatform.Android, new[] { "application/json" } },
+        });
+
+        public static readonly FilePickerFileType FestivalMap = new(new Dictionary<DevicePlatform, IEnumerable<string>>
+        {
+            { DevicePlatform.WinUI, new[] { ".festivalmap" } }, // windows will restrict it to only ".festivalmap" extensions
+            { DevicePlatform.macOS, new[] { "public.item" } },  // Show all files, rely on extension check
+            { DevicePlatform.iOS, new[] { "public.item" } },    // Same as above
+            { DevicePlatform.Android, new[] { "application/octet-stream" } } // All unknown binary types
         });
     }
 }
