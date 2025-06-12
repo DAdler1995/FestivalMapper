d4372991ee83425067207ffee8f346b911dacb8a
Dakota <Dakota@DESKTOP-CJS08FC>
2025-06-11 17:10:48 -0700

    Added android back logic for exiting out of the modals.

--- FestivalMapper.App/Platforms/Android/MainActivity.cs
+++ FestivalMapper.App/Platforms/Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Activity;
 using FestivalMapper.App.Platforms.Android;
 
 namespace FestivalMapper.App
@@ -11,8 +12,29 @@ namespace FestivalMapper.App
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            OnBackPressedDispatcher.AddCallback(new OnBackPressedCallback(true)
+            {
+                HandleOnBackPressedImpl = () =>
+                {
+                    BackHandler.OnBackPressedOverride?.Invoke();
+                }
+            });
+
+
+
             GlobalLayoutUtil.Initialize();
         }
 
     }
+
+    public class OnBackPressedCallback : AndroidX.Activity.OnBackPressedCallback
+    {
+        public Action HandleOnBackPressedImpl { get; set; }
+
+        public OnBackPressedCallback(bool enabled) : base(enabled) { }
+
+        public override void HandleOnBackPressed() => HandleOnBackPressedImpl?.Invoke();
+    }
+
 }
